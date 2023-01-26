682ec7ae652ad235bef24ef3d36bcd580b76476d
Vlasceanu Sebastian <sebastianvlasceanu@icloud.com>
2023-01-26 20:30:40 +0200

    version

--- src/Grid/Controllers/AbstractGridController.php
+++ src/Grid/Controllers/AbstractGridController.php
@@ -4,12 +4,12 @@ namespace Abacus\Grid\Grid\Controllers;
 
 use Abacus\Grid\Grid\Resources\GridResource;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Collection;
 
 abstract class AbstractGridController extends Controller
 {
@@ -19,7 +19,18 @@ abstract class AbstractGridController extends Controller
     {
     }
 
-    abstract public function index(): GridResource;
+    public function edit(string $ids): Model|Collection
+    {
+        $ids = explode(',', $ids);
 
-    abstract public function edit(string $ids): Model|Collection;
+        if (count($ids) === 1) {
+            return $this->builder->findOrFail($ids[0])->toGridFormat();
+        }
+
+        return $this->builder->whereIn('id', $ids)->get()->map(function ($t) {
+            return $t->toGridFormat();
+        });
+    }
+
+    abstract public function index(): GridResource;
 }
